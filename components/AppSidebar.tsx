e23b0a6bed33e222c8179414d58c171c786a511b
CillyDev <abdulaziz.nabaa@gmail.com>
2026-06-07 11:44:42 +0200

    updated icons

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -21,8 +21,8 @@ export function AppSidebar() {
       <SidebarHeader>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-          <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
-              <Image src="/images/raven.png" alt="AccountInfo" width={32} height={32} />
+            <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
+              <Image src="/images/raven.png" alt="AccountInfo" width={30} height={30} />
               Cillian
               <ChevronDown className="ml-auto" />
             </Button>
@@ -39,14 +39,14 @@ export function AppSidebar() {
           </DropdownMenuContent>
         </DropdownMenu>
         <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="w-full">
-        <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
-            <Image src="/images/business-report_6439018.png" alt="homeIcon" width={32} height={32} />
+          <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
+            <Image src="/images/writing_3883016.png" alt="homeIcon" width={30} height={30} />
             Overview
           </Button>
         </Link>
         <Link href="/highlights">
-        <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
-            <Image src="/images/spotlight_8560250.png" alt="showHighlights" width={32} height={32} />
+          <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
+            <Image src="/images/spotlight_8560250.png" alt="showHighlights" width={30} height={30} />
             Highlights
           </Button>
         </Link>
@@ -56,12 +56,31 @@ export function AppSidebar() {
           <Separator className="bg-[#373B3F]"></Separator>
         </span>
       </span>
+
       <SidebarContent>
         <SidebarGroup>
           <Link href="/music-lounge">
             <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
-              <Image src="/images/vinyl-record_2987300.png" alt="homeIcon" width={32} height={32} />
+              <Image src="/images/vinyl-record_2987300.png" alt="homeIcon" width={30} height={30} />
               Music Lounge
+            </Button>
+          </Link>
+          <Link href="/music-lounge">
+            <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
+              <Image src="/images/campfire_9069797.png" alt="homeIcon" width={30} height={30} />
+              Kinship
+            </Button>
+          </Link>
+          <Link href="/music-lounge">
+            <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
+              <Image src="/images/treasure_3016780.png" alt="homeIcon" width={30} height={30} />
+              Shared Secrets 
+            </Button>
+          </Link>
+          <Link href="/music-lounge">
+            <Button className="w-full flex bg-sidebar rounded-md text-default-text text-sm py-[1.5em] items-center justify-start font gap-x-3 hover:bg-hovered-button hover:text-hovered-text">
+              <Image src="/images/scroll_4001989.png" alt="homeIcon" width={30} height={30} />
+              Quest Board
             </Button>
           </Link>
 
